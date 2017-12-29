e72053c2bf30a0d8f777268e8988c0117beb4669
bruno77176 <b.moise@outlook.com>
2017-12-29 22:07:18 +0100

    added an HTML template

--- indexView.php
+++ indexView.php
@@ -1,39 +1,34 @@
-<!-- Affichage -->
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mon blog</title>
-        <link href="style.css" rel="stylesheet" /> 
-    </head>
-        
-    <body>
-        <h1>Mon super blog !</h1>
-        <p>Derniers billets du blog :</p>
+<?php $title = 'Mon blog';?>
 
-        <?php 
-        while ($data = $posts->fetch())
-        {
-        ?>
+<?php ob_start(); ?>
 
-        <div class="news">
-            <h3>
-                <?= htmlspecialchars($data['title']) ?>
-                <em>le <?= $data['creation_date_fr'] ?></em>
-            </h3>
-            
-            <p>
-                <?= nl2br(htmlspecialchars($data['content']))?>
-                <br />
-                <em><a href="post.php?id=<?= $data['id'] ?>">Commentaires</a></em>
-            </p>
-        </div>
+<h1>Mon super blog !</h1>
+<p>Derniers billets du blog :</p>
 
-        <?php
-        } 
-        $posts->closeCursor();
-        ?>
+<?php 
+while ($data = $posts->fetch())
+{
+?>
 
-    </body>
-</html>
+<div class="news">
+    <h3>
+        <?= htmlspecialchars($data['title']) ?>
+        <em>le <?= $data['creation_date_fr'] ?></em>
+    </h3>
+    
+    <p>
+        <?= nl2br(htmlspecialchars($data['content']))?>
+        <br />
+        <em><a href="post.php?id=<?= $data['id'] ?>">Commentaires</a></em>
+    </p>
+</div>
+
+<?php
+} 
+$posts->closeCursor();
+?>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
