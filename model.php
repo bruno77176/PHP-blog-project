a645dd1debf809b48e3f1a8537b7c3ea1f115af7
bruno77176 <b.moise@outlook.com>
2017-12-29 21:19:41 +0100

    refacto connection to database in model

--- model.php
+++ model.php
@@ -2,15 +2,7 @@
 
 function getPosts()
 {
-	try
-	{
-	    $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-	}
-
-	catch(Exception $e)
-	{
-	    die('Erreur : '.$e->getMessage());
-	}
+	$db = dbConnect();
 
 	$req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
 
@@ -21,14 +13,7 @@ function getPosts()
 
 function getPost($postId)
 {
-    try
-    {
-        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-    }
-    catch(Exception $e)
-    {
-        die('Erreur : '.$e->getMessage());
-    }
+    $db = dbConnect();
 
     $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
     $req->execute(array($postId));
@@ -39,17 +24,24 @@ function getPost($postId)
 
 function getComments($postId)
 {
-    try
-    {
-        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-    }
-    catch(Exception $e)
-    {
-        die('Erreur : '.$e->getMessage());
-    }
+    $db = dbConnect();
 
     $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
     $comments->execute(array($postId));
 
     return $comments;
+}
+
+
+function dbConnect()
+{
+	try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+        return $db;
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
 }
