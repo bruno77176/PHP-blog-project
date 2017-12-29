daea43cfc00008fa033ce87d15f5767b21dd9c51
bruno77176 <b.moise@outlook.com>
2017-12-29 20:42:52 +0100

    added new feature showing comments in a new view postView

--- model.php
+++ model.php
@@ -18,3 +18,43 @@ function getPosts()
 
 }
 
+
+function getPost($postId)
+{
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8','root','');
+	}
+
+	catch(Exception $e)
+	{
+		die('Erreur : '.$e->getMessage());
+	}
+
+	$req = $bdd->prepare('SELECT id, title, content, DATE_FORMAT(creation_date,\'%d/%m/%Y à %H%i%ss\') AS creation_date_fr FROM posts WHERE id=?');
+	$req->execute(array($postId));
+
+	$post = req->fetch();
+
+	return $post;
+}
+
+function getComments($postId)
+{
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8','root','');
+	}
+
+	catch(Exception $e)
+	{
+		die('Erreur : ').$e->getMessage();
+	}
+
+	$comments = $bdd->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %H%i%ss\') AS comment_date_fr FROM comments WHERE id=? ORDER BY comment_date DESC');
+	$comments->execute(array($postId));
+
+	return $comments;
+
+
+}
---
+++ post.php
@@ -0,0 +1,15 @@
+<?php 
+
+require ('model.php');
+
+if (isset($_GET['id']) && $_GET['id']>0){
+
+	$post = getPost($_GET['id']);
+	$comments = getComments($_GET['id']);
+	require ('postView.php');
+}
+
+else {
+	
+	echo ('Erreur : aucun identifiant de billet envoyé');
+}
