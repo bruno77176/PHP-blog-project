09b8ceea6c253ad09f90186b26adecded280bc88
bruno77176 <b.moise@outlook.com>
2017-12-29 12:02:27 +0100

    separation of index.php in 2 files, one for the view, one for the model

--- index.php
+++ index.php
@@ -13,46 +13,6 @@ catch(Exception $e)
 
 $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
 
+require("affichageAccueil.php");
 ?>
 
-<!-- Affichage -->
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mon blog</title>
-        <link href="style.css" rel="stylesheet" /> 
-    </head>
-        
-    <body>
-        <h1>Mon super blog !</h1>
-        <p>Derniers billets du blog :</p>
-
-         <?php 
-         while ($donnees = $req->fetch())
-            {
-            ?>
-        <div class="news">
-            <h3>
-                <?php echo htmlspecialchars($donnees['titre']); ?>
-                <em>le <?php echo $donnees['date_creation_fr']; ?></em>
-            </h3>
-            
-            <p>
-            <?php
-            
-            echo nl2br(htmlspecialchars($donnees['contenu']));
-            ?>
-            <br />
-            <em><a href="#">Commentaires</a></em>
-            </p>
-        </div>
-
-        <?php
-        } 
-        $req->closeCursor();
-        ?>
-
-    </body>
-</html>
